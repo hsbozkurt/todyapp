c52575584e453ecfb0f221d3fcea45c0feb827f7
Neurovelo <bhakansait@gmail.com>
2024-09-01 01:16:04 +0400

    refactor: Replace the App class to different file

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo'),
+        ),
+        body: const Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
+}
