a1ecd396303fb83d79d63db20e0d94adf81ebb7b
Neurovelo <bhakansait@gmail.com>
2024-09-01 19:10:02 +0400

    refactor: Add app colors class

---
+++ lib/core/config/theme/app_colors.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+/// This class contains all the colors used in the app.
+class AppColors {
+  // Brand colors
+  static const Color brandDark = Color(0xFF11594B); // #11594B
+  static const Color brandDefault = Color(0xFF1C967E); // #1C967E
+  static const Color brandBackground = Color(0xFFF2F9F9); // #F2F9F9
+  static const Color brandFocused = Color(0xFFA9CEC7); // #A9CEC7
+  static const Color brandPressed = Color(0xFF0F4D40); // #0F4D40
+  static const Color brandOutline = Color(0xFFF2F9F9); // #F2F9F9
+
+  // Neutral colors
+  static const Color neutralPrimary = Color(0xFF1D3066); // #1D3066
+  static const Color neutralSecondary = Color(0xFF737FA0); // #737FA0
+  static const Color neutralGhost = Color(0xFFA9B0C5); // #A9B0C5
+  static const Color neutralLine = Color(0xFFE3E6EC); // #E3E6EC
+  static const Color neutralBackground = Color(0xFFF6F7F9); // #F6F7F9
+  static const Color neutralWhite = Color(0xFFFFFFFF); // #FFFFFF
+
+  // Success colors
+  static const Color successDark = Color(0xFF117A7A); // #117A7A
+  static const Color successDefault = Color(0xFF17A1A1); // #17A1A1
+  static const Color successBackground = Color(0xFFE3F3F3); // #E3F3F3
+  static const Color successFocused = Color(0xFFB8D7D7); // #B8D7D7
+  static const Color successPressed = Color(0xFF0F6666); // #0F6666
+  static const Color successOutline = Color(0xFF8BD0D0); // #8BD0D0
+
+  // Error colors
+  static const Color errorDark = Color(0xFFCC3A55); // #CC3A55
+  static const Color errorDefault = Color(0xFFFF486A); // #FF486A
+  static const Color errorBackground = Color(0xFFFFE9ED); // #FFE9ED
+  static const Color errorFocused = Color(0xFFF0C4CC); // #F0C4CC
+  static const Color errorPressed = Color(0xFFB2324A); // #B2324A
+  static const Color errorOutline = Color(0xFFFFA3B4); // #FFA3B4
+
+  // Warning colors
+  static const Color warningDark = Color(0xFFE0740F); // #E0740F
+  static const Color warningDefault = Color(0xFFFD8311); // #FD8311
+  static const Color warningBackground = Color(0xFFFEF0E2); // #FEF0E2
+  static const Color warningFocused = Color(0xFFF6D5B7); // #F6D5B7
+  static const Color warningPressed = Color(0xFFCC6A0E); // #CC6A0E
+  static const Color warningOutline = Color(0xFFFEC188); // #FEC188
+}
