bb43c17b31c1180e5d1be1c69c8d3b253ccc4372
Neurovelo <bhakansait@gmail.com>
2024-09-02 08:18:20 +0400

    feat: Add a theme to app

--- lib/core/config/theme/app_theme.dart
+++ lib/core/config/theme/app_theme.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:todyapp/core/config/theme/app_colors.dart';
 
 /// This class contains the theme data used in the app.
 class AppTheme {
@@ -6,6 +9,13 @@ class AppTheme {
   static final ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
     useMaterial3: true,
+    colorScheme: _colorScheme,
+    textTheme: GoogleFonts.barlowTextTheme(),
+  );
+
+  // Color scheme of the app
+  static final ColorScheme _colorScheme = ColorScheme.fromSeed(
+    seedColor: AppColors.brandDefault,
   );
 
   /// The dark theme data.
