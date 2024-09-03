da6858a1eb1da177f4fe709b9c061571208e462b
Neurovelo <bhakansait@gmail.com>
2024-09-03 10:27:40 +0400

    docs: Add comments

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+/// Home screen
+/// Screen that displays the main content of the app.
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
--- lib/presentation/screens/onboarding.dart
+++ lib/presentation/screens/onboarding.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+/// Onboarding screen
+/// This screen introduces the app to new users.
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
