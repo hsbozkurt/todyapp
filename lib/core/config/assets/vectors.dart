bc631232d1f25360e2742429240f74578a4d5bf0
Neurovelo <bhakansait@gmail.com>
2024-09-07 23:32:06 +0300

    feat: Add vectors class

---
+++ lib/core/config/assets/vectors.dart
@@ -0,0 +1,11 @@
+/// Vectors class is used to store all the vector assets path.
+class Vectors {
+  static const String basePath = 'assets/svg/';
+
+  // Main images
+  static const String logo = '${basePath}logo.svg';
+
+  // Onboarding screen images
+  static const String onboarding1 = '${basePath}onboarding_1.svg';
+  static const String onboarding2 = '${basePath}onboarding_2.svg';
+}
